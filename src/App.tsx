53427cc6ee09975e845d910e33c17409158e76e4
Daniel Antelo <danielantelo@live.com>
2023-11-21 22:26:07 +0000

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { lazy } from "react";
+import { Route, Routes } from "react-router-dom";
+
+const Gallery = lazy(() => import("./pages/Gallery"));
+const Editor = lazy(() => import("./pages/Editor"));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/edit/:imageId" element={<Editor />} />
+      <Route path="/" element={<Gallery />} />
+    </Routes>
   );
 }
 
